49773cf3f978675b84acf406fc9bd4b648200822
hawk0616 <hawk.taishi0616@gmail.com>
2024-02-11 15:15:11 +0900

    conflict解消

---
+++ app/ranking/layout.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next'
+import React from 'react'
+
+export const metadata: Metadata = {
+  title: 'ランキング | hogehoge'
+}
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>
+}
+
+export default Layout
