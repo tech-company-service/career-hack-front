e3b0ec9a739933ad4ee380588447a71bd1ca7333
hawk0616 <hawk.taishi0616@gmail.com>
2024-02-13 00:49:53 +0900

    修正

--- app/components/ServiceCard.tsx
+++ app/components/ServiceCard.tsx
@@ -1,4 +1,3 @@
-// components/ServiceCard.tsx
 import React, { useState } from 'react'
 import ReactModal from 'react-modal'
 import { CompanyService } from '../types/company_service'
@@ -12,12 +11,23 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ service }) => {
   const descriptionBorderLength: number = 20
 
   const formatDate = (dateString: string): string => {
-    return new Date(dateString).toLocaleDateString('ja-JP', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    })
-  }
+    switch (dateString.length) {
+      case 0:
+        return '-';
+      case 4:
+        return `${dateString}年`;
+      case 6:
+        const yearMonthMatch = dateString.match(/(\d{4})(\d{2})/);
+        if (!yearMonthMatch) throw new Error('Invalid date format for year and month');
+        return `${yearMonthMatch[1]}年${parseInt(yearMonthMatch[2], 10)}月`;
+      case 8:
+        const yearMonthDayMatch = dateString.match(/(\d{4})(\d{2})(\d{2})/);
+        if (!yearMonthDayMatch) throw new Error('Invalid date format for year, month, and day');
+        return `${yearMonthDayMatch[1]}年${parseInt(yearMonthDayMatch[2], 10)}月${parseInt(yearMonthDayMatch[3], 10)}日`;
+      default:
+        throw new Error('Invalid date format');
+    }
+  };
 
   const truncateDescription = (description: string): string => {
     return description.length > descriptionBorderLength
