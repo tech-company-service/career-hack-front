b07514a97bbd82d436b76e885db3c8c6da4b8a3a
Daichi-appy <daichi.es47@gmail.com>
2024-02-19 00:29:59 +0900

    メニューアイコン削除

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -12,13 +12,8 @@ export const Header: React.FC<{ className?: string }> = ({ className }) => {
 
   return (
     <div className={`fixed top-0 left-0 w-full bg-white shadow-md z-10 ${className ?? ''}`}>
-      <nav className='container mx-auto px-6 py-3 flex justify-between items-center'>
-        <div className='flex items-center'>
-          <Link href='/'>
-            <Image src='/menu_icon.svg' alt='Menu' width={40} height={40} />
-          </Link>
-        </div>
-        <div className='flex items-center'>
+      <nav className='container mx-auto px-6 py-3 flex items-center'>
+        <div className='ml-auto'>
           {status === 'authenticated' ? (
             <LoggedInUserInfo />
           ) : (
