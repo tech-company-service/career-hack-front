755fdcf6b65720c21df991bf537703c06f0f2bb7
hawk0616 <hawk.taishi0616@gmail.com>
2024-02-23 17:34:53 +0900

    修正

--- app/components/Tabs.tsx
+++ app/components/Tabs.tsx
@@ -1,34 +1,40 @@
 'use client'
 
-import Link from 'next/link';
-import { useState } from 'react';
+import Link from 'next/link'
+import { useState } from 'react'
 
 const Tabs: React.FC = () => {
   interface Tab {
-    name: string;
-    path: string;
+    name: string
+    path: string
   }
 
   const tabs: Tab[] = [
     { name: '企業', path: '/company' },
     { name: 'ランキング', path: '/ranking' },
-    { name: 'インターン', path: '/intern' }
-  ];
+    { name: 'インターン', path: '/intern' },
+  ]
 
-  const [activeTab, setActiveTab] = useState<Tab>(tabs[0]);
+  const [activeTab, setActiveTab] = useState<Tab>(tabs[0])
 
   return (
-    <div className="flex space-x-4">
-      {tabs.map((tab, index) => (
-        <Link href={tab.path} key={index}>
-          <button
-            className={`py-2 px-4 ${activeTab.path === tab.path ? 'border-b-2 border-black font-bold' : 'border-b-2 border-transparent'}`}
-            onClick={() => setActiveTab(tab)}
-          >
-            {tab.name}
-          </button>
-        </Link>
-      ))}
+    <div className='overflow-x-auto whitespace-nowrap'>
+      <div className='flex space-x-1'>
+        {tabs.map((tab, index) => (
+          <Link href={tab.path} key={index}>
+            <button
+              className={`inline-block py-2 px-4 text-xs sm:text-sm md:text-base rounded ${
+                activeTab.path === tab.path
+                  ? 'bg-blue-400 text-white font-extrabold'
+                  : 'bg-transparent text-black hover:bg-blue-400 hover:text-white'
+              }`}
+              onClick={() => setActiveTab(tab)}
+            >
+              {tab.name}
+            </button>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
